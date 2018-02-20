1717b7f540331f63ce39cc5a31218a19573418fd
PavelKrynskiy <pasha.pele18@gmail.com>
2018-02-20 09:59:18 +0300

    test1

--- src/Test/Test/BinarySearchTreeTest.java
+++ src/Test/Test/BinarySearchTreeTest.java
@@ -1,5 +1,7 @@
 package Test;
 
+import Tree.BinarySearchTree;
+import org.junit.Assert;
 import org.testng.annotations.Test;
 
 import static org.junit.Assert.*;
@@ -8,5 +10,10 @@ public class BinarySearchTreeTest {
 
     @Test
     public void insert() {
+        BinarySearchTree d = new BinarySearchTree();
+        d.insert(5);
+        if (!d.search(5)){
+            Assert.fail();
+        }
     }
 }
